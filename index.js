474c797eaa5a5c5e0522c1a8c437574a316e5896
edward rosario <ed.rosario226@gmail.com>
2017-09-05 19:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var name = "Susan"
 var height = "74"
 if (name === "Susan") {
-  console.log("The heigh is correct")
+  console.log("The name is correct")
+} if (height === "74") {
+console.log("the height is correct")
 } else {
   console.log("Expected the name to be Susan")
 }
